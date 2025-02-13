3ff8b53eb858b0c233b703270f2cc29fcd94c0c9
vaporz <zzxx513@gmail.com>
2025-02-13 16:54:27 +0800

    add rigidbodies and colliders

---
+++ KXLib/Resources.cs
@@ -0,0 +1,22 @@
+﻿using KXRocket;
+using NLog;
+using System;
+using UnityEngine;
+
+namespace KXRocket
+{
+    [ScenePlugin("Resources")]
+    public class Resources : MonoBehaviour
+    {
+        private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        private void Awake()
+        {
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("LiquidFuel", 1));
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("Oxidizer", 1));
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("LqdHydrogen", 1));
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("LqdOxygen", 3));
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("UDMH", 1)); // 偏二甲肼
+            ResourceManager.RegisterResourceType(ResourceType.NewRigidResource("NTO", 1));// 四氧化二氮
+        }
+    }
+}
